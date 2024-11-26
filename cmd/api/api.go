fcbe859e11f18ccc1bcc5ecae85402541f9a3599
raul <raulandrei2017@yahoo.com>
2024-11-26 15:43:20 +0200

    Set up the project

---
+++ cmd/api/api.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"SocialMediaApp/internal/store"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"log"
+	"net/http"
+	"time"
+)
+
+type application struct {
+	config config
+	store  store.Storage
+}
+
+type config struct {
+	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	addr         string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
+}
+
+func (app *application) mount() *chi.Mux {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.RealIP)
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+
+	mux.Use(middleware.Timeout(60 * time.Second))
+
+	mux.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return mux
+}
+
+func (app *application) run(mux *chi.Mux) error {
+	srv := http.Server{
+		Addr:         app.config.addr,
+		Handler:      mux,
+		WriteTimeout: time.Second * 30,
+		ReadTimeout:  time.Second * 10,
+		IdleTimeout:  time.Minute,
+	}
+
+	log.Printf("Server has started at %s", app.config.addr)
+
+	return srv.ListenAndServe()
+}
